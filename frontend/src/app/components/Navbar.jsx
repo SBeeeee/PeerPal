f695d3270f2b6a85cdb14be010aa86de37462a88
SBeeeee <shatadru017@gmail.com>
2025-04-21 21:30:28 +0530

    pushed base code

---
+++ frontend/src/app/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { IoHome } from "react-icons/io5";
+function Navbar() {
+  return (
+    <div className="flex justify-center">
+    <div className="flex bg-blue-600 p-2 px-4 rounded-2xl mb-2 justify-between  w-[90vw]">
+    <IoHome />
+    <IoHome /><IoHome /><IoHome /><IoHome />
+    </div></div>
+  )
+}
+
+export default Navbar
---
+++ frontend/src/app/page.js
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import "./globals.css";
+
+export default function Home() {
+  return (
+   
+      
+  <div className="flex flex-col justify-between h-screen">
+      <div>hello</div>
+      <Navbar/>
+  </div>
+  );
+}
