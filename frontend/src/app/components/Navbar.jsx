717beebd6ae6fb5cb74067ed82e0949238277628
SBeeeee <shatadru017@gmail.com>
2025-04-26 14:46:44 +0530

    initiated dashboard page

--- frontend/src/app/components/Navbar.jsx
+++ frontend/src/app/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Home, Search, PlusCircle, MessageSquare, User } from "lucide-react"
+import Link from 'next/link'
 
 function Navbar() {
   return (
@@ -10,7 +11,7 @@ function Navbar() {
       <Search size={30} strokeWidth={0.75}/>
       <PlusCircle size={30} strokeWidth={0.75}/>
       <MessageSquare size={30} strokeWidth={0.75}/>
-      <User size={30} strokeWidth={0.75}/>
+     <Link href="/dashboard"> <User size={30} strokeWidth={0.75}/></Link>
       
    </div>
   )
---
+++ frontend/src/app/dashboard/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      yet to be made sir 
+      
+    </div>
+  )
+}
+
+export default page
