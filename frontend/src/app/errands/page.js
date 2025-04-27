7191c8726c2af77cef3e45138c9556f8d295cb4b
SBeeeee <shatadru017@gmail.com>
2025-04-27 13:58:25 +0530

    build error fixed

--- frontend/src/app/errands/page.js
+++ frontend/src/app/errands/page.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { errands } from './components/data'
-import Dropdown from './components/DropDown'
+
 
 function page() {
   return (
     <div className="bg-zinc-700">
       {errands[0].title}
-   <Dropdown/>
+  
     </div>
   )
 }
