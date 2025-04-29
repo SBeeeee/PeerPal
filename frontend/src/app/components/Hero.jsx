dcacf5cf9fb1339e65f26f2dbea5a655c60ca6de
SBeeeee <shatadru017@gmail.com>
2025-04-29 16:20:40 +0530

    added formats

--- frontend/src/app/components/Hero.jsx
+++ frontend/src/app/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {ArrowRight} from "lucide-react"
+import { ArrowRight } from "lucide-react"
 import Link from 'next/link'
 
 function Hero() {
@@ -7,8 +7,8 @@ function Hero() {
     <div className="text-center px-4 pt-14">
       <div className="text-white text-3xl mb-2 text-center font-extrabold">Welcome to <span className="text-green-400">HelpBuddy</span></div>
       <div className="text-gray-400 text-lg">Connect with your community to help each other and share resources. Earn points, complete tasks, and rent items.</div>
-    <Link href="/errands"> <button className="bg-green-500 w-[80vw] hover:cursor-pointer hover:bg-green-400 active:scale-95 shadow-2xl rounded-lg border-x-4 border-x-zinc-950  mb-2 border border-gray-950 text-gray-900 mt-2 p-2 ">Browse Tasks <ArrowRight className="inline-block"/></button></Link> 
-      <button className="text-white rounded-lg shadow-2xl border-x-4 mb-6 border border-white p-2 w-[80vw] ">Browse Rentals <ArrowRight className="inline-block"/></button>
+      <Link href="/errands"> <button className="bg-green-500 w-[80vw] hover:cursor-pointer hover:bg-green-400 active:scale-95 shadow-2xl rounded-lg border-x-4 border-x-zinc-950  mb-2 border border-gray-950 text-gray-900 mt-2 p-2 ">Browse Tasks <ArrowRight className="inline-block" /></button></Link>
+      <button className="text-white rounded-lg shadow-2xl border-x-4 mb-6 border border-white p-2 w-[80vw] ">Browse Rentals <ArrowRight className="inline-block" /></button>
     </div>
   )
 }
