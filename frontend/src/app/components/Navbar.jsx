dc4abbd5fb59836f3f874f3e1f6edc8d7fdb3c2d
SBeeeee <shatadru017@gmail.com>
2025-05-14 18:16:41 +0530

    added routes in down navbar

--- frontend/src/app/components/Navbar.jsx
+++ frontend/src/app/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Home, Search, PlusCircle, MessageSquare, User } from "lucide-react"
+import { Home, Car, User,Package,ListTodo } from "lucide-react"
 import Link from 'next/link'
+
 
 function Navbar() {
   return (
@@ -8,9 +9,9 @@ function Navbar() {
     <div className="flex justify-around bg-zinc-900 items-center h-12 fixed bottom-0 mb-2 border-2 text-gray-300  w-[90%] rounded-3xl left-1/2 -translate-x-1/2 border-amber-50">
       
      <Link href="/"><Home size={30} strokeWidth={0.75}/></Link> 
-      <Search size={30} strokeWidth={0.75}/>
-      <PlusCircle size={30} strokeWidth={0.75}/>
-      <MessageSquare size={30} strokeWidth={0.75}/>
+    <Link href="/TripSync"><Car size={40} strokeWidth={0.75}/></Link>
+    <Link href="/rentals"><Package size={30} strokeWidth={0.75}/></Link>
+    <Link href="/errands"><ListTodo size={30} strokeWidth={0.75}/></Link>
      <Link href="/dashboard"> <User size={30} strokeWidth={0.75}/></Link>
       
    </div>
